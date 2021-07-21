df15425179b6bbd4e0862f940de841b160bb5e62
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2021-07-21 16:00:16 +0900

    Add files via upload

---
+++ step1/cat.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+#output
+'''
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+'''
+
