59c541a0eda8c4a1624206c59932f644442dfb08
Kyle Yeo <69660509+hoonkiyeo@users.noreply.github.com>
2021-07-28 16:37:30 +0900

    Add files via upload

---
+++ step6/def_sum.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#sol_1
+def sum_of_n(a):
+    total = 0
+    for num in a:
+        total += num
+    return total
+
+#input
+'''
+sum_of_n([1,2,3,4,5])
+'''
+#output
+'''
+15
+'''
+
+
+# In[ ]:
+
+
+#sol_2
+def solve(a):
+
+    return sum(a)
+
+#input
+'''
+sum_of_n([1,2,3,4,5])
+'''
+#output
+'''
+15
+'''
+'''
+
