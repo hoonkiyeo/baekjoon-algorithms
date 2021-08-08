f343d014379250a87545e0221480890989906588
hoonkiyeo <hky1721@gmail.com>
2021-08-09 00:54:56 +0900

    complete a+b

---
+++ step8/a+b.py
@@ -0,0 +1,14 @@
+#time_limit = 1 sec
+
+a,b = map(int,input().split())
+result = a+b
+print(result)
+
+#input
+'''
+9223372036854775807 9223372036854775808
+'''
+#output
+'''
+18446744073709551615
+'''
