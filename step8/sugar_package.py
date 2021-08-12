425a7a35cdd509adb7d58f1da525a01d80ccd141
hoonkiyeo <hky1721@gmail.com>
2021-08-12 23:47:30 +0900

    solved: sugar package problem

---
+++ step8/sugar_package.py
@@ -0,0 +1,21 @@
+sugar = int(input())
+
+bag = 0
+while sugar >= 0 :
+    if sugar % 5 == 0:
+        bag += (sugar // 5)
+        print(bag)
+        break
+    sugar -= 3
+    bag += 1
+else :
+    print(-1)
+
+#input
+'''
+18
+'''
+#output
+'''
+4
+'''
