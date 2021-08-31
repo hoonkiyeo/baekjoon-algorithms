46e8d26ee1a17ceaaffc38ee2e1d4f22202c5de7
hoonkiyeo <hky1721@gmail.com>
2021-08-31 17:20:26 +0900

    modify apt.py

--- step8/apt.py
+++ step8/apt.py
@@ -1,4 +1,3 @@
-
 import sys
 
 rep = int(input())
@@ -6,25 +5,27 @@ rep = int(input())
 for _ in range(rep):
     floor = int(sys.stdin.readline()) #floor
     num = int(sys.stdin.readline()) #room_number
-    resident = [i for i in range(1,num+1)] #first floor
-    if floor == 0:
-        print(resident[-1])
+    resident_list = [i for i in range(1,num+1)] #first floor
+    if floor == 0: #zero floor
+        print(resident_list[-1])
     for k in range(floor):
         for i in range(1,num):
-            resident[i] += resident[i-1]
-    print(resident[-1])
+            resident_list[i] += resident_list[i-1]
+    print(resident_list[-1])
+
+
 #input
 '''
 2
-1
-3
-2
-3
+1 first floor  
+3 room number 3
+2 second floor
+3 room number 3
 '''
 #output
 '''
-6
-10
+6 residents in room number 3 on floor 1
+10 residents in room number 3 on floor 2
 '''
 
 
