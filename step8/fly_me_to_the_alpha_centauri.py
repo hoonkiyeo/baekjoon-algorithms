c277c5b2e8cbb1ec74d01737d0d77441cafd91e8
hoonkiyeo <hky1721@gmail.com>
2021-09-12 21:49:17 -0500

    update fly me to the alpha centauri.py

---
+++ step8/fly_me_to_the_alpha_centauri.py
@@ -0,0 +1,25 @@
+import math
+import sys
+
+rep = int(input())
+
+for _ in range(rep):
+    x,y = map(int,sys.stdin.readline().split())
+    distance = y - x
+    cnt = 0
+
+    num = math.floor(math.sqrt(distance))
+    num_square = num ** 2
+    increase_num = math.sqrt(num_square)
+
+    if distance > num_square + increase_num:
+        cnt = 2 * num + 1
+    elif distance > num_square and distance <= num_square + increase_num:
+        cnt = 2 * num
+    elif distance == num_square:
+        cnt = 2 * num - 1
+
+    if distance < 4:
+        cnt= distance
+
+    print(cnt)
