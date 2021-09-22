db390ccfe485ed74aa95204322f4346c00c6502c
hoonkiyeo <hky1721@gmail.com>
2021-09-21 22:33:47 -0500

    count_prime_num algorithm updated

---
+++ step9/find_prime_num.py
@@ -0,0 +1,27 @@
+N = int(input()) # N <= 100
+cnt = 0
+num_list = list(map(int,input().split()))
+
+def is_prime(num):
+    if num == 2 or num == 3:
+        return True
+    if num % 2 == 0 or num == 1:
+        return False
+    for i in range(3, int(num**1/2)+1, 2):
+        if num % i == 0:
+            return False
+    return True
+for num in num_list:
+    if is_prime(num) == True:
+        cnt += 1
+print(cnt)
+
+#input
+'''
+4
+1 3 5 7
+'''
+#output
+'''
+3
+'''
