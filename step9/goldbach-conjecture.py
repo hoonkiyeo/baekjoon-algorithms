88987905e42f88fd043bd5925df0631dcb6e286d
hoonkiyeo <hky1721@gmail.com>
2021-10-14 13:18:24 -0500

    update goldbach's conjecture algorithm

--- step9/goldbach-conjecture.py
+++ step9/goldbach-conjecture.py
@@ -1,36 +1,18 @@
 import sys
-import math
 
-def isPrime(num):
-    a = int(math.sqrt(num))
-    if num == 1:
+def isPrime(n):
+    if n < 2:
         return False
-    else:
-        for i in range(2, a+1):
-            if num % i == 0:
-                return False
-        return True
-
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 rep = int(input())
-prime_list = []
 
-for i in range(rep):
+for _ in range(rep):
     n = int(sys.stdin.readline())
-    for i in range(2, n):
-        if isPrime(i):
-            prime_list.append(i)
-
-    for idx in range(len(prime_list)):
-        if n % prime_list[idx] == 0:
-            print(idx,idx,sep=" ")
-        else:
-            num = n % prime_list[idx]
-            print(idx,num,sep=" ")
-
-    prime_list.clear()
-
-
-
-
-
+    for i in range(n // 2, 2, -1):
+        if isPrime(i) and isPrime(n-i):
+            print(i, n-i)
+            break
