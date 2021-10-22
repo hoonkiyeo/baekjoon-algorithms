c651f9fdde95afb7dc804e84c37269626ee06c33
hoonkiyeo <hky1721@gmail.com>
2021-10-21 20:39:19 -0500

    modify solution

--- step_11/find_generator.py
+++ step_11/find_generator.py
@@ -1,13 +1,12 @@
-num = int(input())
-
+num = int(input()) #target number (find generator of this num)
+result = 0
 
 for i in range(1, num+1):
-    digit_sum = sum(list(map(int, str(i))))
-    result = i + digit_sum
-    if result == num:
-        print(i)
+    digit_sum = sum(list(map(int, str(i)))) #if the number is 256, then the digit_sum is 13
+    tot_sum = i + digit_sum
+    if tot_sum == num:
+        result = i
         break
-    if i == num:
-        print(0)
 
+print(result)
 
