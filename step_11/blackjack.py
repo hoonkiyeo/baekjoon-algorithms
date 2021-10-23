dee41b07df9a5563966d12b6570e0d810fb6f5f6
hoonkiyeo <hky1721@gmail.com>
2021-10-22 20:24:26 -0500

    modify blackjack algorithm

--- step_11/blackjack.py
+++ step_11/blackjack.py
@@ -5,15 +5,13 @@ a = list(map(int, input().split())) #five cards
 
 sum = 0
 
-for i in range(N):
-    for j in range(i+1, N):
-        for k in range(j+1, N):
+for i in range(N): #i = 0, 1, 2,...,N-1
+    for j in range(i+1, N): #j = 1, 2,...,N-1
+        for k in range(j+1, N): #k = 2,3,...,N-1
             if a[i] + a[j] + a[k] > M:
                 continue
             else:
                 sum = max(sum, a[i] + a[j] + a[k])
-
-
 
 print(sum)
 
