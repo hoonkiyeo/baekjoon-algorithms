56efc86b4240cdc3ed74d0d21b7146f64bba8e8c
hoonkiyeo <hky1721@gmail.com>
2021-11-03 20:23:00 -0500

    update README.md and modify assigning_room algorithm

---
+++ greedy/README.md
@@ -0,0 +1,11 @@
+### Greedy Algorithms
+
+---
+- Greedy algorithm indicates any simple, intuitive algorithm that is used in optimization problems.
+- The greedy algorithm makes the optimal result at each single step as it attempts to find the overall optimal way to solve the problem.
+
+---
+### Most challenging problem
+```python
+
+```
--- greedy/assigning_room.py
+++ greedy/assigning_room.py
@@ -3,18 +3,16 @@ import sys
 n = int(sys.stdin.readline())
 time_list = []
 cnt = 1
-
 for i in range(n):
     start, end = map(int,sys.stdin.readline().split())
     time_list.append((start,end))
 
 time_list.sort(key = lambda x: (x[1], x[0]))
-
-standard = time_list[0][1]
-for i in range(1, n):
-    if time_list[i][0] >= standard:
+meeting_time = time_list[0]
+for i in range(1, len(time_list)):
+    if time_list[i][0] >= meeting_time[1]:
         cnt += 1
-        standard = time_list[i][1]
+        meeting_time = time_list[i]
 
-print(cnt)
-
+if __name__ == "__main__":
+    print(cnt)
