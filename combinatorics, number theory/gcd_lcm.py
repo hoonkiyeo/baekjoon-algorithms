da013db1e5cb7aa82700bf2742fe0113b807b300
hoonkiyeo <hky1721@gmail.com>
2021-11-19 23:56:37 -0600

    modify number theory/gcd_lcm.py and update basic_comparison.py

--- combinatorics, number theory/gcd_lcm.py
+++ combinatorics, number theory/gcd_lcm.py
@@ -14,3 +14,17 @@ if __name__ == "__main__":
     a, b = map(int, sys.stdin.readline().split())
     print(gcd(a,b))
     print(lcm(a,b))
+
+
+
+import sys
+while True:
+    x,y = map(int, sys.stdin.readline().split())
+    if x <= y:
+        print("No")
+    else:
+        print("Yes")
+    if x == 0 and y == 0:
+        break
+
+
