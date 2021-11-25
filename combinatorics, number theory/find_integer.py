83f0e906506b64a8b387fab9a4ab80f1910c137b
hoonkiyeo <hky1721@gmail.com>
2021-11-24 23:58:55 -0600

    modify find_integer.py

--- combinatorics, number theory/find_integer.py
+++ combinatorics, number theory/find_integer.py
@@ -1,28 +1,30 @@
 # runtime error code
-def check_integer(lst, n):
-    rem_list = []
-    for N in lst:
-        remainder = N % n
-        rem_list.append(remainder)
-    rem_list = list(set(rem_list))
-    if len(rem_list) == 1:
-        return True
-    return False
 
-n = int(sys.stdin.readline())
-num_list = []
-final_list = []
+# def check_integer(lst, n):
+#     rem_list = []
+#     for N in lst:
+#         remainder = N % n
+#         rem_list.append(remainder)
+#     rem_list = list(set(rem_list))
+#     if len(rem_list) == 1:
+#         return True
+#     return False
+#
+# n = int(sys.stdin.readline())
+# num_list = []
+# final_list = []
+#
+# for i in range(n):
+#     num = int(sys.stdin.readline())
+#     num_list.append(num)
+#
+# for i in range(2, max(num_list)):
+#     if check_integer(num_list, i):
+#         final_list.append(i)
+#
+# for ans in final_list:
+#     print(ans, end=" ")
 
-for i in range(n):
-    num = int(sys.stdin.readline())
-    num_list.append(num)
-
-for i in range(2, max(num_list)):
-    if check_integer(num_list, i):
-        final_list.append(i)
-
-for ans in final_list:
-    print(ans, end=" ")
 
 import sys
 import math
