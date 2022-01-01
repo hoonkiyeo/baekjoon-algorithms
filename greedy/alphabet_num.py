e4b180e769696989bea37121b5e7a5fb632bfaec
hoonkiyeo <hky1721@gmail.com>
2022-01-01 06:07:17 -0600

    upload alphabet_num.py

--- greedy/alphabet_num.py
+++ greedy/alphabet_num.py
@@ -1,10 +1,28 @@
 import sys
 
 n = int(input())
-alpha_dic = {}
-input_list = []
+word_list = []
+dic = {}
+ans = 0
 
 for _ in range(n):
-    input_list.append(sys.stdin.readline())
+    word_list.append(sys.stdin.readline().rstrip())
 
+for words in word_list:
+    size = len(words)
+    for word in words:
+        if word not in dic:
+            dic[word] = (10**(size-1))
+        else:
+            dic[word] += (10**(size-1))
+        size -= 1
 
+value_list = list(dic.values())
+value_list.sort(reverse=True)
+
+num = 9
+for value in value_list:
+    ans += value * num
+    num -= 1
+
+print(ans)
